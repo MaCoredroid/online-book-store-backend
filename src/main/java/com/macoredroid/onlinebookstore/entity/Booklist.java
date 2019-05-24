eb10e4cc86faa1aa572af01c4867337be2c2f3b3
macoredroid <coredroid0401@gmail.com>
2019-05-24 12:36:07 +0800

    add MVC

---
+++ src/main/java/com/macoredroid/onlinebookstore/entity/Booklist.java
@@ -0,0 +1,12 @@
+package com.macoredroid.onlinebookstore.entity;
+import com.fasterxml.jackson.annotation.*;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+public class Booklist {
+
+}
