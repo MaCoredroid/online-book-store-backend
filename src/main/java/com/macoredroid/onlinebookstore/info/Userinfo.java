fd718a7eb4b383190e705b18148cfdbfa0f0caa7
macoredroid <coredroid0401@gmail.com>
2019-06-13 12:10:24 +0800

    add user info

---
+++ src/main/java/com/macoredroid/onlinebookstore/info/Userinfo.java
@@ -0,0 +1,12 @@
+package com.macoredroid.onlinebookstore.info;
+
+public class Userinfo {
+    public int userID;
+    public String email;
+    public Userinfo(int userID,String email)
+    {
+        this.email=email;
+        this.userID=userID;
+    }
+
+}
