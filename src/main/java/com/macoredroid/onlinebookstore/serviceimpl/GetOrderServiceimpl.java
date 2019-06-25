90eb3de6d0f6bc3267fe6c3759d00d46b551c086
macoredroid <coredroid0401@gmail.com>
2019-06-25 12:30:10 +0800

    add auto fetch info for order

--- src/main/java/com/macoredroid/onlinebookstore/serviceimpl/GetOrderServiceimpl.java
+++ src/main/java/com/macoredroid/onlinebookstore/serviceimpl/GetOrderServiceimpl.java
@@ -3,6 +3,7 @@ package com.macoredroid.onlinebookstore.serviceimpl;
 import com.macoredroid.onlinebookstore.dao.BooklistDao;
 import com.macoredroid.onlinebookstore.dao.OrderDao;
 import com.macoredroid.onlinebookstore.dao.UserDao;
+import com.macoredroid.onlinebookstore.entity.Booklist;
 import com.macoredroid.onlinebookstore.entity.Order;
 import com.macoredroid.onlinebookstore.entity.User;
 import com.macoredroid.onlinebookstore.info.Orderinfo;
@@ -32,7 +33,16 @@ public class GetOrderServiceimpl implements GetOrderService {
         }
         List<Order> templist= OrderDao.findAllByUserid(tempUser.getUserID());
         for(Order temporder:templist) {
-            resultlist.add(new Orderinfo(temporder.getTime(),temporder.getOrderid(),temporder.getIsbn(),temporder.getNumber(),temporder.getAuthor(),temporder.getName(),temporder.getPrice(),username,temporder.getUserid(),temporder.getBookid()));
+            int tempbookid=temporder.getBookid();
+            Booklist tempbook=BooklistDao.findOne(tempbookid);
+            if(tempbook==null)
+            {
+                resultlist.add(new Orderinfo(temporder.getTime(), temporder.getOrderid(), temporder.getIsbn(), temporder.getNumber(), temporder.getAuthor(), temporder.getName(), temporder.getPrice(), username, temporder.getUserid(), temporder.getBookid()));
+            }
+            else
+            {
+                resultlist.add(new Orderinfo(temporder.getTime(), temporder.getOrderid(), tempbook.getIsbn(), temporder.getNumber(), tempbook.getAuthor(), tempbook.getName(), temporder.getPrice(), username, temporder.getUserid(), temporder.getBookid()));
+            }
         }
 
         return resultlist;
@@ -43,7 +53,27 @@ public class GetOrderServiceimpl implements GetOrderService {
         List<Orderinfo> resultlist= new ArrayList();
         List<Order> templist= OrderDao.findAll();
         for(Order temporder:templist) {
-            resultlist.add(new Orderinfo(temporder.getTime(),temporder.getOrderid(),temporder.getIsbn(),temporder.getNumber(),temporder.getAuthor(),temporder.getName(),temporder.getPrice(),temporder.getUsername(),temporder.getUserid(),temporder.getBookid()));
+            int tempbookid=temporder.getBookid();
+            Booklist tempbook=BooklistDao.findOne(tempbookid);
+            int tempuserid = temporder.getUserid();
+            User tempuser=UserDao.findById(tempuserid);
+            if(tempbook==null&&tempuser==null)
+            {
+                resultlist.add(new Orderinfo(temporder.getTime(),temporder.getOrderid(),temporder.getIsbn(),temporder.getNumber(),temporder.getAuthor(),temporder.getName(),temporder.getPrice(),temporder.getUsername(),temporder.getUserid(),temporder.getBookid()));
+            }
+            if(tempbook==null&&tempuser!=null)
+            {
+                resultlist.add(new Orderinfo(temporder.getTime(),temporder.getOrderid(),temporder.getIsbn(),temporder.getNumber(),temporder.getAuthor(),temporder.getName(),temporder.getPrice(),tempuser.getUsername(),temporder.getUserid(),temporder.getBookid()));
+            }
+            if(tempbook!=null&&tempuser==null)
+            {
+                resultlist.add(new Orderinfo(temporder.getTime(),temporder.getOrderid(),tempbook.getIsbn(),temporder.getNumber(),tempbook.getAuthor(),tempbook.getName(),temporder.getPrice(),temporder.getUsername(),temporder.getUserid(),temporder.getBookid()));
+            }
+            if(tempbook!=null&&tempuser!=null)
+            {
+                resultlist.add(new Orderinfo(temporder.getTime(),temporder.getOrderid(),tempbook.getIsbn(),temporder.getNumber(),tempbook.getAuthor(),tempbook.getName(),temporder.getPrice(),tempuser.getUsername(),temporder.getUserid(),temporder.getBookid()));
+            }
+
         }
         return resultlist;
     }
@@ -57,7 +87,28 @@ public class GetOrderServiceimpl implements GetOrderService {
         }
         else
         {
-            Orderinfo res = new Orderinfo(temporder.getTime(), temporder.getOrderid(), temporder.getIsbn(), temporder.getNumber(), temporder.getAuthor(), temporder.getName(), temporder.getPrice(), temporder.getUsername(), temporder.getUserid(), temporder.getBookid());
+            int tempbookid=temporder.getBookid();
+            Booklist tempbook=BooklistDao.findOne(tempbookid);
+            int tempuserid = temporder.getUserid();
+            User tempuser=UserDao.findById(tempuserid);
+            Orderinfo res=new Orderinfo();
+            if(tempbook==null&&tempuser==null)
+            {
+                res =new Orderinfo(temporder.getTime(),temporder.getOrderid(),temporder.getIsbn(),temporder.getNumber(),temporder.getAuthor(),temporder.getName(),temporder.getPrice(),temporder.getUsername(),temporder.getUserid(),temporder.getBookid());
+            }
+            if(tempbook==null&&tempuser!=null)
+            {
+                res =new Orderinfo(temporder.getTime(),temporder.getOrderid(),temporder.getIsbn(),temporder.getNumber(),temporder.getAuthor(),temporder.getName(),temporder.getPrice(),tempuser.getUsername(),temporder.getUserid(),temporder.getBookid());
+            }
+            if(tempbook!=null&&tempuser==null)
+            {
+               res =new Orderinfo(temporder.getTime(),temporder.getOrderid(),tempbook.getIsbn(),temporder.getNumber(),tempbook.getAuthor(),tempbook.getName(),temporder.getPrice(),temporder.getUsername(),temporder.getUserid(),temporder.getBookid());
+            }
+            if(tempbook!=null&&tempuser!=null)
+            {
+                res =new Orderinfo(temporder.getTime(),temporder.getOrderid(),tempbook.getIsbn(),temporder.getNumber(),tempbook.getAuthor(),tempbook.getName(),temporder.getPrice(),tempuser.getUsername(),temporder.getUserid(),temporder.getBookid());
+            }
+
             return res;
         }
     }
