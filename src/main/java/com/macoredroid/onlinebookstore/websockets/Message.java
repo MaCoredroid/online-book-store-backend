1e76d698dc660cd45035ea3178c1dc1001088dce
macoredroid <coredroid0401@gmail.com>
2020-03-21 13:18:55 +0800

    add websocket

---
+++ src/main/java/com/macoredroid/onlinebookstore/websockets/Message.java
@@ -0,0 +1,15 @@
+package com.macoredroid.onlinebookstore.websockets;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
